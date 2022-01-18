1e7ed45b316822b4f4a779f2e639601404d07000
Young Soo Koh <kohys92@gmail.com>
2022-01-18 23:39:05 +0900

    data abstraction - specifying

--- StudentTranscript/src/model/Transcript.java
+++ StudentTranscript/src/model/Transcript.java
@@ -2,7 +2,8 @@ package model;
 
 public class Transcript {
 
-    //REQUIRES: nothing
+    //REQUIRES: the grade should be between 0.0 and 4.0, and/or
+    //          the course should not be null
     //MODIFIES: this
     //EFFECTS: adds a grade into a transcript
     public void addGrade(String course, double grade){
@@ -11,20 +12,21 @@ public class Transcript {
 
     //REQUIRES: the list should not be empty
     //MODIFIES: nothing
-    //EFFECTS: return course name and grade in some consistent String format
+    //EFFECTS: return course name and grade in format CourseName: Grade
     public String getCourseAndGrade(String course){
         return null;
     }
 
     //REQUIRES: nothing
     //MODIFIES: nothing
-    //EFFECTS: prints a transcript with course names and grades with student info
+    //EFFECTS: prints a transcript with course names and grades
+    //         and displays student academic record
     public void printTranscript(){
         //stub
     }
 
     //REQUIRES: nothing
-    //MODIFIES: this
+    //MODIFIES: nothing
     //EFFECTS: calculates the GPA and returns the GPA
     public double getGPA(){
         return 0.0;
