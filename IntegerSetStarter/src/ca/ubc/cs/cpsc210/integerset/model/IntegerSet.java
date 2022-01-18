5a2e9f58e593417673b990b500c0ee825050abe2
Young Soo Koh <kohys92@gmail.com>
2022-01-18 18:46:50 +0900

    implementing IntegerSet

--- IntegerSetStarter/src/ca/ubc/cs/cpsc210/integerset/model/IntegerSet.java
+++ IntegerSetStarter/src/ca/ubc/cs/cpsc210/integerset/model/IntegerSet.java
@@ -6,34 +6,39 @@ import java.util.ArrayList;
  * Represents a set of integers
  */
 public class IntegerSet {
+    ArrayList<Integer> internalArray;
 
     // EFFECTS: make an empty set
     public IntegerSet() {
-        //stub
+        internalArray = new ArrayList<>();
 
     }
 
     // EFFECTS: return the number of integers in the set
     public int size () {
-        return 0;
+        return internalArray.size();
 
     }
 
     // EFFECTS: return true if an Integer is in the set, false otherwise
     public boolean contains (Integer anInteger) {
-        return false;
+        return internalArray.contains(anInteger);
     }
 
     // MODIFIES: this
     // EFFECTS: insert an Integer into the set, if it is not already there,
     public void insert(Integer anInteger) {
-        //stub
+        if(!internalArray.contains(anInteger)){
+            internalArray.add(anInteger);
+        }
     }
 
     // REQUIRES: an Integer is in the set (contains(anInteger) is true)
     // MODIFIES: this
     // EFFECTS: remove anInteger from the set
     public void remove(Integer anInteger) {
-        // stub
+        if(internalArray.contains(anInteger)){
+            internalArray.remove(anInteger);
+        }
     }
 }
