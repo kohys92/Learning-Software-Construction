5d777869d2ed5cdef7116aec3207e910edc43fe5
Young Soo Koh <kohys92@gmail.com>
2022-01-18 23:30:56 +0900

    Data Abstraction with specifications

---
+++ StudentTranscript/src/model/Transcript.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Transcript {
+
+    //REQUIRES: nothing
+    //MODIFIES: this
+    //EFFECTS: adds a grade into a transcript
+    public void addGrade(String course, double grade){
+        //stub
+    }
+
+    //REQUIRES: the list should not be empty
+    //MODIFIES: nothing
+    //EFFECTS: return course name and grade in some consistent String format
+    public String getCourseAndGrade(String course){
+        return null;
+    }
+
+    //REQUIRES: nothing
+    //MODIFIES: nothing
+    //EFFECTS: prints a transcript with course names and grades with student info
+    public void printTranscript(){
+        //stub
+    }
+
+    //REQUIRES: nothing
+    //MODIFIES: this
+    //EFFECTS: calculates the GPA and returns the GPA
+    public double getGPA(){
+        return 0.0;
+    }
+
+}
